6296e5817bce25f2907e5f2fab2928e81718d4cd
Cezary Grzanka <cezary_grzanka@epam.com>
2024-04-08 15:52:41 +0200

    Fix api paths

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
-  product: "https://am0wgycpvh.execute-api.eu-central-1.amazonaws.com/dev",
-  order: "https://am0wgycpvh.execute-api.eu-central-1.amazonaws.com/dev",
-  import: "https://am0wgycpvh.execute-api.eu-central-1.amazonaws.com/dev",
-  bff: "https://am0wgycpvh.execute-api.eu-central-1.amazonaws.com/dev",
-  cart: "https://am0wgycpvh.execute-api.eu-central-1.amazonaws.com/dev",
+  product: "https://d4vjqt5c36.execute-api.eu-central-1.amazonaws.com/dev",
+  order: "https://d4vjqt5c36.execute-api.eu-central-1.amazonaws.com/dev",
+  import: "https://d4vjqt5c36.execute-api.eu-central-1.amazonaws.com/dev",
+  bff: "https://d4vjqt5c36.execute-api.eu-central-1.amazonaws.com/dev",
+  cart: "https://d4vjqt5c36.execute-api.eu-central-1.amazonaws.com/dev",
 };
 
 export default API_PATHS;
